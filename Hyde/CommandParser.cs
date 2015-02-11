72d7ec9379b4d9067e0e8b67a0304126ac9d8807
Brandon Wood <bcwood@gmail.com>
2015-02-11 07:37:13 -0900

    initial project structure, basic command parsing

---
+++ Hyde/CommandParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Hyde
+{
+	public enum CommandType
+	{
+		Build,
+		Help,
+		Unknown
+	}
+
+	public class Command
+	{
+		public CommandType Type { get; set; }
+		public string[] Args { get; set; }
+
+		public static Command Parse(string[] args)
+		{
+			if (!args.Any())
+			{
+				return null;
+			}
+
+			var commandArg = args[0];
+			var command = new Command();
+
+			switch (commandArg)
+			{
+				case "build":
+					command.Type = CommandType.Build;
+					break;
+
+				case "help":
+					command.Type = CommandType.Help;
+					break;
+
+				default:
+					command.Type = CommandType.Unknown;
+					break;
+			}
+
+			// TODO: parse command options
+
+			return command;
+		}
+
+		public void Execute()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Hyde/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hyde
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var command = Command.Parse(args);
+
+			if (command == null)
+			{
+				Console.WriteLine("No command specified.");
+				Console.WriteLine("Usage: hyde <command> [options]");
+				Console.WriteLine("Use 'hyde help' for a list of available commands.");
+				return;
+			}
+			else if (command.Type == CommandType.Unknown)
+			{
+				Console.WriteLine("Unknown command: " + args[0]);
+				Console.WriteLine("Use 'hyde help' for a list of available commands.");
+				return;
+			}
+
+			command.Execute();
+		}
+	}
+}
